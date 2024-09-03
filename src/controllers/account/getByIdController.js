954f87880fcdd4b590f3a5149921eb30dc6056c5
Allanaksp <karolineallana8@gmail.com>
2024-09-03 17:25:49 -0300

    add delete and update

--- src/controllers/account/getByIdController.js
+++ src/controllers/account/getByIdController.js
@@ -1,19 +1,19 @@
-import { getByIdAccounts } from "../../models/accountModel.js"
+import { getByIdAccount } from "../../models/accountModel.js"
 
 const getById = async (req, res) => {
-    //const id = req.params.id//
-    const {id, slug} = req.params
-    const account = await getById(+id)
+    //const id = req.params.id
+    const {id} = req.params
+    const account = await getByIdAccount(+id)
 
     if(!account)
         return res.status(404).json({
-            error: 'Conta com o id ${id}, não encontrado!'
-    })
+            error: `Conta com o id ${id}, não encontrado!`
+        })
 
-    res.json({
-        success: "Conta encontrada com Suceso!", 
+    return res.json({
+        success: "Conta encontrada com sucesso!",
         account
     })
 }
 
-export default getById  
+export default getById
